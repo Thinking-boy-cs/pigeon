bf0f6bf55d71a9ff35ef9822df1053a4dce31bdb
Yusw <1499356850@qq.com>
2020-11-15 17:35:22 +0800

    Auto stash before merge of "dev" and "origin/dev"

--- back/src/main/java/cn/swu/pigeon/PigeonApplication.java
+++ back/src/main/java/cn/swu/pigeon/PigeonApplication.java
@@ -1,5 +1,6 @@
 package cn.swu.pigeon;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- back/src/main/java/cn/swu/pigeon/controller/MainController.java
+++ back/src/main/java/cn/swu/pigeon/controller/MainController.java
@@ -8,27 +8,63 @@ package cn.swu.pigeon.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import cn.swu.pigeon.entity.User;
 
-@RestController
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+@Controller
 public class MainController {
 
-    @ResponseBody
-    @RequestMapping("/test")
-    public List<User> test()
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @RequestMapping("/page")
+    public String loginPage()
     {
-        List<User> userList = new ArrayList<User>();
-        for(int i = 0; i < 10; i ++)
-        {
-            User user = new User();
-            user.setId("ddd" + i);
-            user.setName("banana" + i);
-            userList.add(user);
+        return "login";
+    }
+    @RequestMapping("/checkLogin")
+    @ResponseBody
+    //public boolean getDbType(@RequestBody(required=false) User user) {
+    public boolean getDbType() {
+        //String theId = user.getId();
+        //String thePwd = user.getPassword();
+        String theId = "001";
+        String thePwd = "123";
+        System.out.println(theId);
+        System.out.println(thePwd);
+        String sql = "select * from staff";
+        List list = jdbcTemplate.queryForList(sql);
+
+        Iterator iterator = list.iterator();
+        while (iterator.hasNext()) {
+            Map userMap = (Map) iterator.next();
+
+
+            if (userMap.get("id").equals(theId) && userMap.get("passWord").equals(thePwd)) {
+                System.out.println("登录验证成功！！！");
+                return true;
+            } else {
+                System.out.println("登录验证失败！！！");
+                return false;
+            }
+
         }
-        return userList;
+        return false;
     }
 }
+
+
+
