1c17f2dc6c63c74be8503bdff202721c8b3e971f
M.Scarlet <452602057@qq.com>
2020-11-15 18:43:20 +0800

    ADD:Notification.vue

--- front/webapp/src/router/index.js
+++ front/webapp/src/router/index.js
@@ -8,27 +8,32 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import LoginPage from '@/components/LoginPage'
 import Register from '@/components/Register'
+import Notification from '@/components/Notification'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
 
 Vue.use(Router)
 Vue.use(Antd)
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: LoginPage
-    },
-    {
-      path: '/register',
-      name: 'Regist',
-      component: Register
-    }
+  routes: [{
+    path: '/',
+    name: 'HelloWorld',
+    component: HelloWorld
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginPage
+  },
+  {
+    path: '/register',
+    name: 'Regist',
+    component: Register
+  },
+  {
+    path: '/Notification',
+    name: 'Notification',
+    component: Notification
+  }
   ]
 })
