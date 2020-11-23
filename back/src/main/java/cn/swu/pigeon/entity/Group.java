594d62ee4c3a9cb4d726707e7aae0f99b84ca5c8
Jecosine <mj_ol@outlook.com>
2020-11-24 00:12:23 +0800

    ADD: Add rabbitmq

--- back/src/main/java/cn/swu/pigeon/entity/Group.java
+++ back/src/main/java/cn/swu/pigeon/entity/Group.java
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2020-11-23 21:26:43
- * @LastEditTime: 2020-11-23 21:29:11
- * @LastEditors: your name
+ * @LastEditTime: 2020-11-23 23:17:43
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \back\src\main\java\cn\swu\pigeon\entity\Group.java
  */
@@ -10,8 +10,11 @@ package cn.swu.pigeon.entity;
 
 import java.io.Serializable;
 
+import org.springframework.stereotype.Component;
+
 import lombok.Data;
 
+@Component
 @Data
 public class Group implements Serializable {
     private String id;
