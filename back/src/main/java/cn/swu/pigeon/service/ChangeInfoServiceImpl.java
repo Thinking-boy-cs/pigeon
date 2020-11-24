0644f7d3651d09c9c2650fd58e732f75373eb8b2
Yusw <1499356850@qq.com>
2020-11-24 23:48:49 +0800

    UPDATE: Code before step

--- back/src/main/java/cn/swu/pigeon/service/ChangeInfoServiceImpl.java
+++ back/src/main/java/cn/swu/pigeon/service/ChangeInfoServiceImpl.java
@@ -3,6 +3,7 @@ package cn.swu.pigeon.service;
 import cn.swu.pigeon.dao.ChangeInfoDAO;
 import cn.swu.pigeon.entity.User;
 import cn.swu.pigeon.utils.UploadUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,10 +20,11 @@ public class ChangeInfoServiceImpl implements ChangeInfoService{
     ChangeInfoDAO changeInfoDAO;
 
     HttpServletRequest request;
+    //String path = request.getServletPath();
     //public static final String UPLOAD_DIR = httpServletRequest.getServletPath()+"../../../resources/static/photos";
 //    public static final String UPLOAD_DIR = "../../../resources/static/photos";
     //public static final String UPLOAD_DIR = request.getServletContext().getRealPath("/photos");
-    public static final String UPLOAD_DIR = "webapp\\upload";
+    public static final String UPLOAD_DIR = "./back/src//main/resources/static/photos";
 
     private UploadUtils uploadUtils = new UploadUtils();
 
@@ -31,6 +33,7 @@ public class ChangeInfoServiceImpl implements ChangeInfoService{
     public void changeUserInfo(MultipartFile multipartFile,User user) {
         String newPath = uploadUtils.upload(UPLOAD_DIR,multipartFile);
         System.out.println(newPath);
+        //System.out.println(path);
         user.setIcon(newPath);
         try {
             changeInfoDAO.changeInfo(user);
