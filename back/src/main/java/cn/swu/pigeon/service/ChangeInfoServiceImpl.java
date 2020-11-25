b1f14342dc6b47a604f7bb6283627ffc43ab2ddf
Yusw <1499356850@qq.com>
2020-11-25 16:37:31 +0800

    ！！！

--- back/src/main/java/cn/swu/pigeon/service/ChangeInfoServiceImpl.java
+++ back/src/main/java/cn/swu/pigeon/service/ChangeInfoServiceImpl.java
@@ -39,7 +39,7 @@ public class ChangeInfoServiceImpl implements ChangeInfoService{
             changeInfoDAO.changeInfo(user);
         } catch (Exception e){
             e.printStackTrace();
-        }
+        }   
 
     }
 }
