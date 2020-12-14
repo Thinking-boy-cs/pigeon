7acaaefead577749b8d4a3e68a133fa96f629343
M.Scarlet <452602057@qq.com>
2020-12-14 21:54:58 +0800

    UPDATE

--- front/backfront/src/router/index.js
+++ front/backfront/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Antd from 'ant-design-vue/es'
+import 'ant-design-vue/dist/antd.less'
 Vue.use(Router)
 Vue.use(Antd)
 export default new Router({
