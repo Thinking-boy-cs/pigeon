73675b2fc00b6df5a8dd2984492886c44f3c4041
Yusw <1499356850@qq.com>
2020-12-15 17:19:06 +0800

    UPDATE: The view of let on or off

--- back/src/main/java/cn/swu/pigeon/controller/StatisticsViewController.java
+++ back/src/main/java/cn/swu/pigeon/controller/StatisticsViewController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -131,7 +133,7 @@ public class StatisticsViewController {
     @RequestMapping("salary_userNumber")
     public Map<String,Object> statisticsSalaryUserNumber(String companyId){
         //测试
-        //companyId = "123456789012345";
+        companyId = "123456789012345";
 
         Map<String,Object> map = new HashMap<>();
         try {
@@ -140,6 +142,35 @@ public class StatisticsViewController {
                 map.put("status",0);
                 map.put("msg","查看成功");
                 map.put("data",sl);
+            } else {
+                map.put("status",1);
+                map.put("msg","查看失败");
+            }
+            return map;
+        } catch (Exception e){
+            e.printStackTrace();
+            map.put("status",1);
+            map.put("msg","查看失败");
+            return map;
+        }
+    }
+
+    //员工在离职情况统计
+    @RequestMapping("letOperation_userNumber")
+    public Map<String,Object> statisticsLetUserNumber(){
+        //测试
+        String companyId = "123456789012345";
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        //String time = simpleDateFormat.format(tDate);
+        String time = "2020-12-15";
+        Map<String,Object> map = new HashMap<>();
+        try {
+            if(!ObjectUtils.isEmpty(companyId)){
+                List<StatisticsCount> sp = viewDataService.viewStatusNumber(companyId,time);
+                map.put("status",0);
+                map.put("msg","查看成功");
+                map.put("data",sp);
             } else {
                 map.put("status",1);
                 map.put("msg","查看失败");
