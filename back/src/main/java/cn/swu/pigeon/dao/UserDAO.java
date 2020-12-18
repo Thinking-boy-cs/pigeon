47701a7a02540e49849dd7ffa2508aa1e9711b04
griow <1445055880@qq.com>
2020-12-18 21:30:47 +0800

    UPDATE: 登录返回map,但没有测试

--- back/src/main/java/cn/swu/pigeon/dao/UserDAO.java
+++ back/src/main/java/cn/swu/pigeon/dao/UserDAO.java
@@ -3,10 +3,12 @@ package cn.swu.pigeon.dao;
 import cn.swu.pigeon.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.Map;
+
 @Mapper //用来创建DAO对象
 public interface UserDAO {
     //注册存入用户
     void save(User user);
 
-    User findByTel(String telNumber);
+    Map findByTel(String telNumber);
 }
